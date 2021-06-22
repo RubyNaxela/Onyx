a0d518822adcd1fafa40196250328449bdc16bb0
RubyNaxela <rubynaxela@gmail.com>
2021-06-22 12:04:10 +0200

    Updated copyright headers

--- src/main/java/com/rubynaxela/onyx/gui/GUIManager.java
+++ src/main/java/com/rubynaxela/onyx/gui/GUIManager.java
@@ -1,18 +1,12 @@
 /*
- * Copyright (c) 2021 Jacek Pawelski a.k.a. RubyNaxela
+ * Copyright (c) 2021 RubyNaxela
+ * All Rights Reserved
  *
- * Licensed under the GNU General Public License v3.0 (the "License");
- * you may not use this file except in compliance with the License.
- * The license is included in file 'LICENSE.txt', which is part of this
- * source code package. You may also obtain a copy of the License at
+ * This file is part of the Onyx project.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
  *
- *      https://www.gnu.org/licenses/gpl-3.0.txt
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Written by Jacek Pawelski <rubynaxela@gmail.com>
  */
 
 package com.rubynaxela.onyx.gui;
